3a529092cc9d6896acc045ea42f683370232bc10
Chandrakanth Santharam <chandrakanth.162@gmail.com>
2025-06-01 21:03:11 +0530

    Delete houmanager directory

--- houmanager/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import BundlesTag, Bundles
-admin.site.register(BundlesTag)
-admin.site.register(Bundles)
--- houmanager/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class HoumanagerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'houmanager'
